91a7e80e2af93ec47872d46633997ac6a05f07a2
Alberto Macias <alberto.macbar9512@gmail.com>
2020-07-19 20:43:33 -0500

    removed

--- ThirdAssingment/rankhospital.R
+++ ThirdAssingment/rankhospital.R
@@ -11,19 +11,8 @@ rankhospital <- function(state, outcome, num){
       my_data <- subset(outcome_2, State == state, select = c("hospital",outcome))
       my_data <- arrange(my_data, my_data[,2], my_data[,1])
       available <- sum(complete.cases(my_data[,2]))
+      num <- as.integer(num)
       
-      if (num == "best"){
-            ranked <- my_data[1,1]
-      }
-      
-      else if (num == "worst"){
-            ranked <- my_data[1,available]
-      }
-      
-     
-      else{
-            ranked <- my_data[1,num]
-      }
-      
-      ranked
+      my_data[1,1]
+    
 }
