03d29a811930ed881a4c8d6b09bf829e65c5adc4
Alberto Macias <alberto.macbar9512@gmail.com>
2020-07-20 20:02:18 -0500

    rankhospital created succesfully

--- rankhospital.R
+++ rankhospital.R
@@ -1,18 +1,54 @@
 rankhospital <- function(state, outcome, num){
       
-      if(is.element(state,outcome_2$State)==FALSE){
-            stop("invalid state")
-      } 
-      
+  ## Read outcome data   
+      outcome_data <- read.csv("outcome-of-care-measures.csv", na.strings = "Not Available")
+      outcome_names <- c("heart attack", "heart failure", "pneumonia")
+  
+  
+  ## Check that state and outcome are valid
+      if(is.element(state,outcome_data$State)==FALSE){
+          stop("invalid state")
+        } 
+  
       if(is.element(outcome,outcome_names)==FALSE){
-            stop("invalid outcome")
+          stop("invalid outcome")
       }
       
-      my_data <- subset(outcome_2, State == state, select = c("hospital",outcome))
-      my_data <- arrange(my_data, my_data[,2], my_data[,1])
-      available <- sum(complete.cases(my_data[,2]))
-      num <- as.integer(num)
+      ## Return hospital name in that state with the given rank
+      ## 30-day death rate
       
-      my_data[1,1]
-    
+      if(outcome == outcome_names[1]){
+        col_num <- 11
+      }
+      
+      if(outcome == outcome_names[2]){
+        col_num <- 17
+      }
+      
+      if(outcome == outcome_names[3]){
+        col_num <- 23
+      }
+      
+      my_data <- subset(outcome_data, State == state, select = c(2,col_num))
+      my_data_2 <- arrange(my_data, my_data[,2], my_data[,1])
+      available <- sum(complete.cases(my_data[,2]))
+      
+      if(num == "best"){
+        ranked <- my_data_2[1,1]
+      }
+      
+      if(num == "worst"){
+        ranked <- my_data_2[available,1]
+      }
+      
+      if(is.numeric(num)==TRUE & num>available){
+        message("num is greater than observations")
+        return(NA)
+      }
+      
+      if(is.numeric(num)==TRUE){
+        ranked <- my_data_2[num,1]
+      }
+      
+      return(ranked)
 }
